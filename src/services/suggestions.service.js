dc55b5c0355a305a44af3935bab9f04cb56fcceb
Chantal Steur <chant_steur@hotmail.com>
2019-05-06 07:34:47 +0200

    #Exercise-3 fix lint errors

--- src/services/suggestions.service.js
+++ src/services/suggestions.service.js
@@ -1,4 +1,4 @@
-const API_URL_SUGGESTIONS = 'http://localhost:3000/search\?q\=';
+const API_URL_SUGGESTIONS = 'http://localhost:3000/search?q=';
 
 const getSuggestions = async (query) => {
   const response = await fetch(API_URL_SUGGESTIONS + query, {
